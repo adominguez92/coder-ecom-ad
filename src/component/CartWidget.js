d7d7fe60bd27f151ca7fa9289815eac3d39c150b
Agustin Dominguez <agndoz@gmail.com>
2022-08-18 21:05:27 -0300

    add: Logo y CartWidget
    Se agrega logo.ico como logo de la pagina y se crea el CartWidget.js en component

---
+++ src/component/CartWidget.js
@@ -0,0 +1,14 @@
+
+const CartWidget = () =>
+{
+    return(
+    <>
+        <a class="navbar-brand" href="">
+          <img src="..\public\logo.ico" alt="" width="30" height="24" class="d-inline-block align-text-top" />
+          Box Store
+        </a>
+    </>
+    );
+}
+
+export default CartWidget;
