dccf04695b7767e878725a57834067f1019cdb04
Agustin Dominguez <agndoz@gmail.com>
2022-08-22 19:16:32 -0300

    Se agrega cambio para agregar icono en NavBar

--- src/component/CartWidget.js
+++ src/component/CartWidget.js
@@ -1,10 +1,11 @@
+import logo from './logo.ico'
 
 const CartWidget = () =>
 {
     return(
     <>
         <a class="navbar-brand" href="">
-          <img src="..\public\logo.ico" alt="" width="30" height="24" class="d-inline-block align-text-top" />
+          <img src={logo} alt="" width="30" height="24" class="d-inline-block align-text-top" />
           Box Store
         </a>
     </>
